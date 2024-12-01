882f5c360d3701f685f81400e437786f82037546
Jose F7 <recuerdaporquelohaces@gmail.com>
2024-12-01 17:14:09 -0500

    version 1.4.5 add URL FRONTEND

--- src/main/java/Package/PHARMACY_PROJECT/config.java
+++ src/main/java/Package/PHARMACY_PROJECT/config.java
@@ -1,4 +1,5 @@
 package Package.PHARMACY_PROJECT;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,11 +10,13 @@ public class config implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .exposedHeaders("Content-Disposition", "Authorization"); // Exponer encabezados específicos
-
+                .allowedOrigins(
+                        "http://localhost:4200", // Para desarrollo local
+                        "https://farmacia-proyecto-front.vercel.app" // Dominio desplegado en Vercel
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .exposedHeaders("Content-Disposition", "Authorization") // Exponer encabezados
+                .allowCredentials(true); // Permitir envío de cookies o credenciales si es necesario
     }
-
 }
